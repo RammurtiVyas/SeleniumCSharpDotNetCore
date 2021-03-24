dedcc35e67faf92387053c93f31d229c166fe477
RammurtiVyas <Rammurti.Vyas@live.com>
2021-03-24 03:02:35 +0000

    Check-in the code

---
+++ SeleniumCSharpDotNetCore/CustomControl.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Threading;
+
+namespace SeleniumCSharpDotNetCore
+{
+    public class CustomControl : DriverHelper
+    {
+        IWebDriver _driver;
+        public CustomControl(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public void ComboBox(string name, string value)
+        {
+            
+            var combo = _driver.FindElement(By.XPath($"//*[@id='{name}-awed']"));
+            combo.Clear();
+            combo.SendKeys(value);
+            combo.Click();
+            combo.SendKeys(Keys.Enter);
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ SeleniumCSharpDotNetCore/UnitTest1.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SeleniumCSharpDotNetCore
+{
+    public class Tests : DriverHelper
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Navigate().GoToUrl("https://demo.aspnetawesome.com/");
+            Driver.FindElement(By.Id("Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.XPath("//input[@name='ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+            CustomControl custom = new CustomControl(Driver);
+            custom.ComboBox("AllMealsCombo", "Mango");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Thread.Sleep(3000);
+            Driver.Quit();
+        }
+    }
+}
